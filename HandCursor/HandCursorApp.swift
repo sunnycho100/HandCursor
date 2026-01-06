a6c8be589625f0c788ee99d8491b8d78c38dfc8b
sunnycho100 <studysung1880@gmail.com>
2026-01-06 20:53:19 +0900

    Initial Commit

---
+++ HandCursor/HandCursorApp.swift
@@ -0,0 +1,17 @@
+//
+//  HandCursorApp.swift
+//  HandCursor
+//
+//  Created by Sunghwan Cho on 1/6/26.
+//
+
+import SwiftUI
+
+@main
+struct HandCursorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
